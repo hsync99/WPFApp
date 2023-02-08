50d5bde3f8c5cdc4e11ffa420d57d58b7b79308f
Leonid Gavrilov <hsync99@gmail.com>
2023-02-08 16:44:26 +0600

    MenuWindow Collection of Items Sample

--- WPFApp/ViewModels/MenuViewModel.cs
+++ WPFApp/ViewModels/MenuViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace WPFApp.ViewModels
 {
@@ -10,9 +11,25 @@ namespace WPFApp.ViewModels
     {
         private string _userid;
         public string userid { get => _userid; set => _userid = value; }
+        private Button _btn;
+        public Button Btn { get=> _btn; set => _btn = value; }  
+        private List<Button> _buttons;
+        public List<Button> Buttons { get=>_buttons; set => _buttons = value; } 
         public MenuViewModel()
         {
-           
+            
+
+
+            Buttons = new List<Button>();
+            for (int i = 0; i < 20; i++)
+            {
+                Btn = new Button();
+                Btn.Height = 50;
+                Btn.Width = 50;
+                Buttons.Add(Btn);
+            }
+            
+
         }
     }
 }
